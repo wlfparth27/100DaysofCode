f6faf4110c6c150e1ee97a817bb2c7b4da582e99
Parth <wlfparth27@gmail.com>
2025-08-30 17:36:16 +0530

    Day 2 --> Question 2

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float radius;
+    printf("Enter Radius: ");
+    scanf("%f", &radius);
+    float circumference = 2 * (22/7) * radius;
+    float area = (22/7) * (radius * radius);
+    printf("Area=%.2f, Circumference=%.2f\n", area, circumference);
+    return 0;
+}
