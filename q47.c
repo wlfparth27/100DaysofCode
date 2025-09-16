05a278b805da08475293edcb3b00b504a553dc90
Parth <wlfparth27@gmail.com>
2025-09-17 01:05:22 +0530

    Question 47

---
+++ q47.c
@@ -0,0 +1,37 @@
+/*
+Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+*/
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
