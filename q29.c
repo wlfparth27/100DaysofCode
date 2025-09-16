f2dfcc6a06a380fc85db93b6151fa442eed4d81f
Parth <wlfparth27@gmail.com>
2025-09-16 23:42:18 +0530

    Question 29

---
+++ q29.c
@@ -0,0 +1,34 @@
+//Q29: Write a program to calculate the factorial of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+
+    printf("%d\n", factorial);
+
+    return 0;
+}
