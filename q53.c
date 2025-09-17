f7f30b97e33c8dda5fa86e50ac6f6000fc480d28
Parth <wlfparth27@gmail.com>
2025-09-17 16:13:07 +0530

    Question 53

---
+++ q53.c
@@ -0,0 +1,46 @@
+/*Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    int pattern[] = {1, 3, 5, 7, 9, 7, 5, 3, 1};
+    int groups = sizeof(pattern) / sizeof(pattern[0]);
+
+    for (i = 0; i < groups; i++) 
+    {
+        for (j = 0; j < pattern[i]; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
