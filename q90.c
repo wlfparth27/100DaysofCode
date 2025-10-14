785373cdb16204cbbacc4a532c5bd256f50d7b10
Parth <wlfparth27@gmail.com>
2025-10-14 21:21:08 +0530

    Question 90

---
+++ q90.c
@@ -0,0 +1,31 @@
+/*Q90: Toggle case of each character in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+hELLO
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A');
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + ('a' - 'A');
+        }
+    }
+
+    printf("Toggled case string: %s", str);
+    return 0;
+}
